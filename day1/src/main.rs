1ba6737874b8e88b7e2875459586417e43346fc7
dejke <kasperdejke@gmail.com>
2023-12-01 19:40:47 +0100

    day1: expect -> unwrap

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,15 +4,15 @@ fn main(){
     let file =  fs::read_to_string("./input").unwrap();
     let lines =  file.lines();
    
-    let sum = lines.map(code_from_line).reduce(|a,b| a+b).expect("Could not find the codes");
+    let sum = lines.map(code_from_line).reduce(|a,b| a+b).unwrap();
     print!("{}", sum);
 }
 
 fn code_from_line(line: &str) -> u32{
     let chars:Vec<char> = line.chars().collect();
 
-    let first_index = line.find(|c:char| c.is_digit(10)).expect("Could not find digits in line");
-    let last_index = line.rfind(|c:char| c.is_digit(10)).expect("Could not find digits in line");
+    let first_index = line.find(|c:char| c.is_digit(10)).unwrap();
+    let last_index = line.rfind(|c:char| c.is_digit(10)).unwrap();
 
     return chars[first_index].to_digit(10).unwrap() * 10 + chars[last_index].to_digit(10).unwrap();
 }
